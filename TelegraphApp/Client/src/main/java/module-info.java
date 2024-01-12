91ab90434e7d43dabfe7f5956bf2a24b2708feea
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-01-12 10:58:18 +0200

    projects initialized.

--- TelegraphApp/Client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module jets.iti.client {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens jets.iti.client to javafx.fxml;
-    exports jets.iti.client;
-}
--- TelegraphApp/Server/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module jets.iti.server {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens jets.iti.server to javafx.fxml;
-    exports jets.iti.server;
-}
