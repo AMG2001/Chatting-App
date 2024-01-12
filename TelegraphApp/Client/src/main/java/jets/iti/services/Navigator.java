8c1ff902f21b4ca3c490ac39cb7739f4d28239b3
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-01-12 12:12:10 +0200

    navigator and dialogs added

---
+++ TelegraphApp/Client/src/main/java/jets/iti/services/Navigator.java
@@ -0,0 +1,24 @@
+package jets.iti.services;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Navigator {
+    // used to show Scene that you want to Move For .
+    static Parent loadedFXML;
+
+    public static void gotToScene(Button btn, String fxmlFilePath) {
+        try {
+            loadedFXML = FXMLLoader.load(Navigator.class.getResource(fxmlFilePath));
+        } catch (Exception e) {
+            System.out.println("Error done with fxmlFilePath loader in Navigation Function while navigating to : " + fxmlFilePath);
+            System.out.println(e.getMessage());
+        }
+        Stage window = (Stage) btn.getScene().getWindow();
+        window.setScene(new Scene(loadedFXML));
+    }
+}
