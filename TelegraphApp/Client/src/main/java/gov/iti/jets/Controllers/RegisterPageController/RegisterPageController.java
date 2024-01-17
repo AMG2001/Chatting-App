cc554a75b2eb07c1d80e2db44de3d415f9484ff2
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-01-17 21:35:03 +0200

    register design changed

--- TelegraphApp/Client/src/main/java/gov/iti/jets/Controllers/RegisterPageController/RegisterPageController.java
+++ TelegraphApp/Client/src/main/java/gov/iti/jets/Controllers/RegisterPageController/RegisterPageController.java
@@ -1,20 +1,60 @@
 package gov.iti.jets.Controllers.RegisterPageController;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import gov.iti.jets.config.AppPages;
 import gov.iti.jets.services.Navigator;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.text.html.ImageView;
 
 public class RegisterPageController {
+
     @FXML
-    Hyperlink link_goToLogin;
+    private Hyperlink link_gotoLogin;
+
     @FXML
-    Button btn_register;
+    private MFXButton btn_register;
+
+    @FXML
+    private ToggleGroup gender;
+
+    @FXML
+    private ImageView img_user;
+
+    @FXML
+    private RadioButton rb_female;
+
+    @FXML
+    private RadioButton rb_male;
+
+    @FXML
+    private TextArea ta_bio;
+
+    @FXML
+    private TextField tf_country;
+
+    @FXML
+    private TextField tf_email;
+
+    @FXML
+    private TextField tf_name;
+
+    @FXML
+    private PasswordField tf_password;
+
+    @FXML
+    private TextField tf_phone;
+
+    @FXML
+    public void pickUserImage(MouseEvent event) {
+
+    }
 
     @FXML
     public void gotoLogin(ActionEvent actionEvent) {
-        Navigator.gotToScene(link_goToLogin, AppPages.getLoginPage());
+        Navigator.gotToScene(link_gotoLogin, AppPages.getLoginPage());
     }
 }
