b6b487407abe60f397159f190b9d58bd702de8f4
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2024-01-19 19:32:37 +0200

    modified

--- TelegraphApp/Server/src/main/java/gov/iti/jets/dao/RowsetFactory.java
+++ TelegraphApp/Server/src/main/java/gov/iti/jets/dao/RowsetFactory.java
@@ -16,69 +16,59 @@ public class RowsetFactory {
     private static javax.sql.rowset.RowSetFactory rsFactory;
 
 
-    public static JdbcRowSet getJDBCRowset()
-    {
-        JdbcRowSet jdbcRowSet =null;
-        try{
+    public static JdbcRowSet getJDBCRowset() {
+        JdbcRowSet jdbcRowSet = null;
+        try {
             rsFactory = RowSetProvider.newFactory();
             jdbcRowSet = rsFactory.createJdbcRowSet();
-
             jdbcRowSet.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
             jdbcRowSet.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
             jdbcRowSet.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
-        }catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return jdbcRowSet;
     }
 
-    public static CachedRowSet getCachedRowset()
-    {
+    public static CachedRowSet getCachedRowset() {
+        // TODO Need to be updated , we will need connection Object Here .
         CachedRowSet cachedRowset = null;
-        try{
+        try {
             rsFactory = RowSetProvider.newFactory();
             cachedRowset = rsFactory.createCachedRowSet();
-
-
             cachedRowset.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
             cachedRowset.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
             cachedRowset.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
 
-        }catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return cachedRowset;
     }
 
-    public static WebRowSet getWebRowset()
-    {
+    public static WebRowSet getWebRowset() {
         WebRowSet webRowSet = null;
-        try{
+        try {
             rsFactory = RowSetProvider.newFactory();
             webRowSet = rsFactory.createWebRowSet();
-
             webRowSet.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
             webRowSet.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
             webRowSet.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
-        }catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return webRowSet;
     }
 
-    public static FilteredRowSet getFilteredRowset()
-    {
+    public static FilteredRowSet getFilteredRowset() {
         FilteredRowSet filteredRowset = null;
         try {
             filteredRowset = rsFactory.createFilteredRowSet();
 
 
-        filteredRowset.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
-        filteredRowset.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
-        filteredRowset.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
+            filteredRowset.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
+            filteredRowset.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
+            filteredRowset.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -86,18 +76,16 @@ public class RowsetFactory {
     }
 
 
-    public static JoinRowSet getJoinRowset()
-    {
+    public static JoinRowSet getJoinRowset() {
         JoinRowSet joinRowset = null;
-        try{
+        try {
             rsFactory = RowSetProvider.newFactory();
             joinRowset = rsFactory.createJoinRowSet();
 
             joinRowset.setUrl(prop.getProperty(PropertiesFileUtil.getDbUrl()));
             joinRowset.setUsername(prop.getProperty(PropertiesFileUtil.getDbUsername()));
             joinRowset.setPassword(prop.getProperty(PropertiesFileUtil.getDbPassword()));
-        }catch(SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return joinRowset;
