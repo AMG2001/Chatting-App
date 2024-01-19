7b1008153fb97164bd7659ec18f19b5b9f81dc7e
Mahmoud Marwan <dark1996live.com>
2024-01-19 21:01:50 +0200

    Various Updates

--- TelegraphApp/Server/src/main/java/gov/iti/jets/Server.java
+++ TelegraphApp/Server/src/main/java/gov/iti/jets/Server.java
@@ -1,12 +1,18 @@
 package gov.iti.jets;
 
-import gov.iti.jets.persistence.rowsetimpl.ContactRequestRowsetImpl;
+import gov.iti.jets.domain.User;
+import gov.iti.jets.persistence.dao.UserDao;
+import gov.iti.jets.persistence.rowsetimpl.UserRowsetImpl;
+
+import java.sql.Date;
+
 public class Server {
     public static void main(String[] args){
 
-        ContactRequestRowsetImpl impl = new ContactRequestRowsetImpl();
-
-
+        UserDao user = new UserRowsetImpl();
+        user.add(new User("12345657765", "hamda", "hamada@hamada.hamda",
+                "password", "Hamada", "ONLINE", "MALE", "Bio",
+                "imageREf", new java.sql.Date(System.currentTimeMillis())));
 
     }
 
--- TelegraphApp/Server/src/main/java/gov/iti/jets/persistence/rowsetimpl/UserRowsetImpl.java
+++ TelegraphApp/Server/src/main/java/gov/iti/jets/persistence/rowsetimpl/UserRowsetImpl.java
@@ -1,15 +1,40 @@
 package gov.iti.jets.persistence.rowsetimpl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import gov.iti.jets.persistence.dao.UserDao;
 import gov.iti.jets.domain.User;
 
-public class UserRowsetImpl implements UserDao {
+import javax.sql.rowset.JdbcRowSet;
 
+public class UserRowsetImpl implements UserDao {
 
     @Override
     public void add(User entity) {
+        String insertStatement = "INSERT INTO User (phone_number, name, email, password, dob, country, gender, bio, picture) VALUES"+
+                " (?,?,?,?,?,?,?,?,?)";
+
+        try{
+            JdbcRowSet rowset = RowsetFactory.getJDBCRowset();
+            rowset.setCommand(insertStatement);
+
+            rowset.setString(1,entity.getPhoneNumber());
+            rowset.setString(2,entity.getName());
+            rowset.setString(3,entity.getEmail());
+            rowset.setString(4,entity.getPassword());
+            rowset.setDate(5,entity.getDob());
+            rowset.setString(6,entity.getCountry());
+            rowset.setString(7,entity.getGender());
+            rowset.setString(8,entity.getBio());
+            rowset.setString(9,entity.getImageReference());
+
+            rowset.execute();
+
+        }catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
 
