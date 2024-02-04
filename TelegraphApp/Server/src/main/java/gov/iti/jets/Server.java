bcf806b67a8ee7b69653cc9d103c15d0ca5995c9
Mahmoud Marwan <dark1996live.com>
2024-02-04 12:24:34 +0200

    Test Commit

--- TelegraphApp/Server/src/main/java/gov/iti/jets/Server.java
+++ TelegraphApp/Server/src/main/java/gov/iti/jets/Server.java
@@ -4,11 +4,6 @@ import gov.iti.jets.services.application.ApplicationServices;
 
 public class Server {
     public static void main(String[] args) {
-        /**
-         * to load and init all application services :
-         * 1. initialize database Connection .
-         * 2. init all rowsets object to be able to perform crud operations on all tables .
-         */
         ApplicationServices.initApplicationServices();
 
     }
